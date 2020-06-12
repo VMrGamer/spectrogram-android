a2da8de887a432ba6e9b1eb09c4d5c78bf5bc072
Vedant Patadia <v.mr.gamer@gmail.com>
2020-06-12 20:08:30 +0530

    Getting a 4s short fileBuffer to convert to WAV file or send through Volley

--- app/src/main/java/net/galmiza/android/spectrogram/SpectrogramActivity.java
+++ app/src/main/java/net/galmiza/android/spectrogram/SpectrogramActivity.java
@@ -23,6 +23,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,6 +33,7 @@ import net.galmiza.android.engine.sound.SoundEngine;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -64,9 +66,13 @@ public class SpectrogramActivity extends AppCompatActivity {
 	
 	// Buffers
 	private List<short[]> bufferStack; // Store trunks of buffers
+	private short[] fileBuffer; // buffer supporting the file generation
 	private short[] fftBuffer; // buffer supporting the fft process
 	private float[] re; // buffer holding real part during fft process
 	private float[] im; // buffer holding imaginary part during fft process
+
+	//file helpers
+	static private int iterVal = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -299,7 +305,7 @@ public class SpectrogramActivity extends AppCompatActivity {
 		
 		// Prepare recorder
 		recorder.prepare(fftResolution); // Record buffer size if forced to be a multiple of the fft resolution
-		
+		fileBuffer = new short[66560];
 		// Build buffers for runtime
 		int n = fftResolution;
 		fftBuffer = new short[n];
@@ -325,8 +331,15 @@ public class SpectrogramActivity extends AppCompatActivity {
 	 * Send these buffers for FFT processing (call to process())
 	 */
 	private void getTrunks(short[] recordBuffer) {
+		iterVal++;
+		for(int i = 0;i<1024;i++) {
+			fileBuffer[iterVal * i] = recordBuffer[i];
+		}
+		if(iterVal == 64){
+			iterVal = 0;
+			Log.d("YOYO", "getTrunks: "+ Arrays.toString(fileBuffer));
+		}
 		int n = fftResolution;
-		
 		// Trunks are consecutive n/2 length samples		
 		for (int i=0; i<bufferStack.size()-1; i++)
 			System.arraycopy(recordBuffer, n/2*i, bufferStack.get(i+1), 0, n/2);
@@ -358,7 +371,7 @@ public class SpectrogramActivity extends AppCompatActivity {
 		nativeLib.shortToFloat(fftBuffer, re, n);
 		nativeLib.clearFloat(im, n);	// Clear imaginary part
 		timeView.setWave(re);
-		
+
 		// Windowing to reduce spectrum leakage
 		String window = Misc.getPreference(
 				this,
@@ -374,7 +387,7 @@ public class SpectrogramActivity extends AppCompatActivity {
 		else if (window.equals("Nuttall"))			nativeLib.windowNuttall(re, n);
 		else if (window.equals("Blackman-Nuttall"))	nativeLib.windowBlackmanNuttall(re, n);
 		else if (window.equals("Blackman-Harris"))	nativeLib.windowBlackmanHarris(re, n);
-		
+
 		nativeLib.fft(re, im, log2_n, 0);	// Move into frquency domain 
 		nativeLib.toPolar(re, im, n);	// Move to polar base
 
@@ -400,4 +413,54 @@ public class SpectrogramActivity extends AppCompatActivity {
 		else											frequencyView.setVisibility(View.GONE);
 	}
 
+	private byte[] wavFileHeader(long totalAudioLen, long totalDataLen, long longSampleRate,
+								 int channels, long byteRate, byte bitsPerSample) {
+		byte[] header = new byte[44];
+		header[0] = 'R'; // RIFF/WAVE header
+		header[1] = 'I';
+		header[2] = 'F';
+		header[3] = 'F';
+		header[4] = (byte) (totalDataLen & 0xff);
+		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+		header[8] = 'W';
+		header[9] = 'A';
+		header[10] = 'V';
+		header[11] = 'E';
+		header[12] = 'f'; // 'fmt ' chunk
+		header[13] = 'm';
+		header[14] = 't';
+		header[15] = ' ';
+		header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+		header[17] = 0;
+		header[18] = 0;
+		header[19] = 0;
+		header[20] = 1; // format = 1
+		header[21] = 0;
+		header[22] = (byte) channels;
+		header[23] = 0;
+		header[24] = (byte) (longSampleRate & 0xff);
+		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+		header[28] = (byte) (byteRate & 0xff);
+		header[29] = (byte) ((byteRate >> 8) & 0xff);
+		header[30] = (byte) ((byteRate >> 16) & 0xff);
+		header[31] = (byte) ((byteRate >> 24) & 0xff);
+		header[32] = (byte) (channels * (bitsPerSample / 8)); //
+		// block align
+		header[33] = 0;
+		header[34] = bitsPerSample; // bits per sample
+		header[35] = 0;
+		header[36] = 'd';
+		header[37] = 'a';
+		header[38] = 't';
+		header[39] = 'a';
+		header[40] = (byte) (totalAudioLen & 0xff);
+		header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+		header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+		return header;
+	}
 }
